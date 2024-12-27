e299df18a02b1f851cbef467cab741a712b1b2ee
Eduk <ekakhrisnanda@gmail.com>
2024-12-27 16:33:27 +0800

    Add some changes and new functions
    Recall display score inside playRound func
    Create a gameOver flag to stop the game
    Create a function checkTheChampion to make sure who's first to reach target score
    Execute gameOver = true if someone reach the target score
    Add the gameOver = true inside event listener

--- script.js
+++ script.js
@@ -10,67 +10,6 @@ const getComputerChoice = () => {
     }
 };
 
-// function loopGame() {
-//     for(; humanScore < targetScore && compScore < targetScore; i++) {
-//         humanSelection = gethumanSelection(result);
-//         computerSelection = getComputerChoice();
-//         playRound(humanSelection, computerSelection);
-//     }
-
-//     if(humanScore >= targetScore) {
-//         console.log("Congratulations, you are the champion!");
-//     } else {
-//         console.log("Computer wins. Better luck next time!");
-//     }
-// }
-
-// function playGame() {
-
-//     const targetScore = 5;
-//     let humanScore = 0;
-//     let compScore = 0;
-
-//     let humanSelection = gethumanSelection();
-//     let computerSelection = getComputerChoice();
-    
-//     function playRound(humanSelection, computerSelection) {
-//         if (humanSelection === computerSelection) {
-//             console.log("Tie!")
-//         } else if (humanSelection === "Rock" && computerSelection === "Scissor" 
-//             || humanSelection === "Paper" && computerSelection === "Rock" 
-//             || humanSelection === "Scissor" && computerSelection === "Paper"
-//         ) {
-//             console.log(`You Win!!! ${humanSelection} beats ${computerSelection}`)
-//             return humanScore++;
-//         } else {
-//             console.log(`You Lose haha. ${computerSelection} beats ${humanSelection}`);
-//             return compScore++;
-//         }
-//     }
-
-//     playRound();
-
-//     function loopGame() {
-//         if (humanScore >= targetScore || compScore >= targetScore) {
-//             if (humanScore >= targetScore) {
-//                 console.log("Congratulations, you are the champion!");
-//             } else {
-//                 console.log("Computer wins. Better luck next time!");
-//             }
-//             return;
-//         } else {
-//             humanSelection = gethumanSelection();
-//             computerSelection = getComputerChoice();
-//             playRound(humanSelection, computerSelection);
-//             loopGame();
-//         }
-//     }
-
-//     loopGame();
-// }
-
-
-
 const targetScore = 5;
 let humanScore = 0;
 let compScore = 0;
@@ -97,32 +36,50 @@ function playRound(humanSelection, computerSelection) {
         || humanSelection === "Scissor" && computerSelection === "Paper"
     ) {
         displayWinner.textContent = `You Win!!! ${humanSelection} beats ${computerSelection}`;
-        return humanScore++;
+        humanScore++;
+        return displayHumanScore.textContent = "Human Score = " + humanScore;
+
     } else {
         displayWinner.textContent = `You Lose haha. ${computerSelection} beats ${humanSelection}`;
-        return compScore++;
+        compScore++;
+        return displayCompScore.textContent = "Computer Score = " + compScore;
+    }
+}
+
+let gameOver = false;
+
+function checkTheChampion() {
+    if (humanScore >= targetScore || compScore >= targetScore) {
+        gameOver = true;
+        if (humanScore >= targetScore) {
+            displayWinner.textContent = "Congratulations, you are the champion!";
+        } else {
+            displayWinner.textContent = "Computer wins. Better luck next time!";
+        }
     }
 }
 
 const buttons = document.querySelectorAll('.button');
 buttons.forEach(button => {
     button.addEventListener('click', e => {
+        if (gameOver) return;
+
         const humanSelection = e.currentTarget.innerText;
         const computerSelection = getComputerChoice();
         playRound(humanSelection, computerSelection);
+        checkTheChampion();
     });
 });
 
-// change the console.logs inside playRound
-// display the winner of the round by insert it through DOM
+// create if statement
+// if human score or comp score reach the target score
+// display the champion
 
-// loop around all the buttons
-// If one of the each button clicked, return the txt content
-// store the return value in a variable for human selection
-// get the computer selection
-// play the game
-// increment score between two if one of has won the round
-// if anybody reach the target score first, announce the winner
+// if (humanScore >= targetScore) {
+//     displayWinner.textContent = "Congratulations, you are the champion!";
+// } else {
+//     displayWinner.textContent = "Computer wins. Better luck next time!";
+// }
 
 
 
